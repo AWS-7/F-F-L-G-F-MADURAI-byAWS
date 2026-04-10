e3ada0add629388dfc1cb269db413bd6ebbf6e92
AWS-TEAM <agniwebsolution@gmail.com>
2026-04-10 09:04:35 +0530

    main

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/F-F-L-G-F-MADURAI-byAWS/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
