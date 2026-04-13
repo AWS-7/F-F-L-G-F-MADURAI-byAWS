0ddd24d1106a6c7c163d20de3584870f312f90a6
AWS-TEAM <agniwebsolution@gmail.com>
2026-04-13 09:38:21 +0530

    Configure for Netlify custom domain femmeflexladiesgym.com - optimize performance

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/F-F-L-G-F-MADURAI-byAWS/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
